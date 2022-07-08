431761381161267624478ef28172031f4d41a160
carlynorama <carlynorama@gmail.com>
2022-07-08 16:57:43 -0700

    removed unused dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
+        //.package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
         //.package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
     ],
     targets: [
